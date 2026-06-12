de32ae21b5efc7fbcf58ab7d5753798f511de4c1
luis-alva0 <luisandres.alvaabanto@gmail.com>
2026-06-11 20:10:29 -0500

    first commit

---
+++ src/main/java/com/techwatch/techwatchbackend/TechwatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.techwatch.techwatchbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechwatchBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechwatchBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techwatch/techwatchbackend/TechwatchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techwatch.techwatchbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechwatchBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
