0581512ac69c4175ed19ac683b5b7a5db96bfc53
luis-alva0 <luisandres.alvaabanto@gmail.com>
2026-06-13 16:59:47 -0500

    chore: configurar base del backend y shared kernel

---
+++ src/main/java/com/techwatch/techwatchbackend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,67 @@
+package com.techwatch.techwatchbackend.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+/**
+ * Configures the OpenAPI specification exposed by the platform.
+ */
+@Configuration
+public class OpenApiConfiguration {
+    // Properties
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Value("${documentation.application.description}")
+    String applicationDescription;
+
+    @Value("${documentation.application.version}")
+    String applicationVersion;
+
+    // Methods
+
+    /**
+     * Builds the OpenAPI document used by Swagger UI and client generation tools.
+     *
+     * @return configured OpenAPI descriptor
+     */
+    @Bean
+    public OpenAPI techWatchPlatformOpenApi() {
+
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title(this.applicationName)
+                        .description(this.applicationDescription)
+                        .version(this.applicationVersion)
+                        .contact(new Contact()
+                                .name("TechWatch Support")
+                                .email("support@techwatch.com")
+                                .url("https://techwatch.com/support"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("TechWatch Platform Documentation")
+                        .url("https://github.com/upc-pre-202610-1asi0729-11896-techwatch"));
+
+        // Add server configurations
+        openApi.servers(List.of(
+                new Server()
+                        .url("http://localhost:8080")
+                        .description("Local Development Environment")
+        ));
+
+        return openApi;
+    }
+}
