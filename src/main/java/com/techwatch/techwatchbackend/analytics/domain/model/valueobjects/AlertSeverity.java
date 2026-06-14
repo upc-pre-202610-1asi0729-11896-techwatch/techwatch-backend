b246060650e3e031df97e978a808ec6e2e0f9e1b
luis-alva0 <luisandres.alvaabanto@gmail.com>
2026-06-14 14:59:35 -0500

    feat(analytics): agregar modelo de dominio de ConsumptionAlert

---
+++ src/main/java/com/techwatch/techwatchbackend/analytics/domain/model/valueobjects/AlertSeverity.java
@@ -0,0 +1,10 @@
+package com.techwatch.techwatchbackend.analytics.domain.model.valueobjects;
+
+/**
+ * The severity of a consumption alert.
+ */
+public enum AlertSeverity {
+    LOW,
+    MEDIUM,
+    HIGH
+}
