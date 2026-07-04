e075b550e6c7e8a2dc1a07f7e04f888639fe78f3
Ronal345 <ronaltt345@gmail.com>
2026-07-04 14:39:04 -0500

    feat(subscriptions): agregar SubscriptionPersistenceRepository (Spring Data)

---
+++ src/main/java/com/techwatch/techwatchbackend/subscriptions/infrastructure/persistence/jpa/repositories/SubscriptionPersistenceRepository.java
@@ -0,0 +1,16 @@
+package com.techwatch.techwatchbackend.subscriptions.infrastructure.persistence.jpa.repositories;
+
+package com.techwatch.techwatchbackend.subscriptions.infrastructure.persistence.jpa.repositories;
+
+import com.techwatch.techwatchbackend.subscriptions.domain.model.valueobjects.SubscriptionStatus;
+import com.techwatch.techwatchbackend.subscriptions.domain.model.valueobjects.UserId;
+import com.techwatch.techwatchbackend.subscriptions.infrastructure.persistence.jpa.entities.SubscriptionPersistenceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface SubscriptionPersistenceRepository extends JpaRepository<SubscriptionPersistenceEntity, Long> {
+    Optional<SubscriptionPersistenceEntity> findByUserIdAndStatus(UserId userId, SubscriptionStatus status);
+}
