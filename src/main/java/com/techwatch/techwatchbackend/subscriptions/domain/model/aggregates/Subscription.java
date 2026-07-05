a1fe4c7704a4924acdd05190e59b9e51e8a27f27
Sebastian Uriel Becerra Durand <u20221b756@upc.edu.pe>
2026-07-04 19:47:50 -0500

    feat(subscriptions): add cancel subscription domain model

--- src/main/java/com/techwatch/techwatchbackend/subscriptions/domain/model/aggregates/Subscription.java
+++ src/main/java/com/techwatch/techwatchbackend/subscriptions/domain/model/aggregates/Subscription.java
@@ -1,6 +1,7 @@
 package com.techwatch.techwatchbackend.subscriptions.domain.model.aggregates;
 
 import com.techwatch.techwatchbackend.shared.domain.model.aggregates.AbstractDomainAggregateRoot;
+import com.techwatch.techwatchbackend.subscriptions.domain.model.events.SubscriptionCanceledEvent;
 import com.techwatch.techwatchbackend.subscriptions.domain.model.events.SubscriptionRenewedEvent;
 import com.techwatch.techwatchbackend.subscriptions.domain.model.valueobjects.SubscriptionStatus;
 import lombok.Getter;
@@ -35,6 +36,14 @@ public class Subscription extends AbstractDomainAggregateRoot<Subscription> {
     public Subscription() {
     }
 
+    public boolean isActive() {
+        return this.status == SubscriptionStatus.ACTIVE;
+    }
+
+    public boolean isExpired() {
+        return this.status == SubscriptionStatus.EXPIRED;
+    }
+
     public boolean isCanceled() {
         return this.status == SubscriptionStatus.CANCELED;
     }
@@ -54,6 +63,22 @@ public class Subscription extends AbstractDomainAggregateRoot<Subscription> {
         this.status = SubscriptionStatus.ACTIVE;
 
         registerDomainEvent(new SubscriptionRenewedEvent(this.id, this.endDate));
+
+        return this;
+    }
+
+    public Subscription cancel() {
+        if (this.isCanceled()) {
+            throw new IllegalStateException("Subscription is already canceled");
+        }
+
+        if (this.isExpired()) {
+            throw new IllegalStateException("Expired subscriptions cannot be canceled");
+        }
+
+        this.status = SubscriptionStatus.CANCELED;
+
+        registerDomainEvent(new SubscriptionCanceledEvent(this.id, LocalDate.now()));
 
         return this;
     }
