598f5ea9fc07ab9ddf7e1384553e32cd6212a8fe
luis-alva0 <luisandres.alvaabanto@gmail.com>
2026-07-05 01:31:43 -0500

    fix(subscriptions): completar SubscriptionsContextFacade con hasFeature

--- src/main/java/com/techwatch/techwatchbackend/subscriptions/interfaces/acl/SubscriptionsContextFacade.java
+++ src/main/java/com/techwatch/techwatchbackend/subscriptions/interfaces/acl/SubscriptionsContextFacade.java
@@ -12,4 +12,14 @@ public interface SubscriptionsContextFacade {
      * @return The maximum number of devices allowed
      */
     int fetchMaxDevicesForUser(Long userId);
+
+    /**
+     * Check whether the plan of the given user includes the given feature.
+     * Users without an active subscription are evaluated against the free plan.
+     *
+     * @param userId The user id
+     * @param feature The feature to check
+     * @return true if the user's plan includes the feature
+     */
+    boolean hasFeature(Long userId, PlanFeature feature);
 }
